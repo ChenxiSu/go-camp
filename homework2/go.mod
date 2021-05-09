e69ded89ede004cbc439d11f492a125ade36051e
ChenxiSu <cs2238@cornell.edu>
2021-05-09 12:45:48 -0400

    use context more properly

--- homework2/go.mod
+++ homework2/go.mod
@@ -2,4 +2,7 @@ module hm2
 
 go 1.16
 
-require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
--- homework2/main.go
+++ homework2/main.go
@@ -11,41 +11,44 @@ import (
 	"time"
 
 	"golang.org/x/sync/errgroup"
+	errors "github.com/pkg/errors"
 )
 
-func serve(addr string, handler http.Handler, stop <-chan struct{} ) error {
+func serve(ctx context.Context, addr string, handler http.Handler) error {
 	srv := http.Server{
 		Addr: addr,
 		Handler: handler,
 	}
 
 	go func() {
-		<- stop
-		fmt.Println("\nClosing server!")
-		srv.Close()
+		select {
+		case <-ctx.Done():
+			fmt.Println("\nClosing server!")
+			srv.Close()
+		}
 	}()
 
 	// Start http server
 	return srv.ListenAndServe()
 }
 
-func serveApp(stop <-chan struct{}) error{
+func serveApp(ctx context.Context) error{
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
 		fmt.Fprintln(resp, "Hello World!")
 	})
 	fmt.Println("Starting app server at 127.0.0.1:8080")
-	return serve("127.0.0.1:8080", mux, stop)
+	return serve(ctx, "127.0.0.1:8080", mux)
 }
 
-func serveDebug(stop <-chan struct{}) error{
+func serveDebug(ctx context.Context) error{
 	fmt.Println("Starting debug server at 127.0.0.1:8001")
-	return serve("127.0.0.1:8001", http.DefaultServeMux, stop)
+	return serve(ctx, "127.0.0.1:8001", http.DefaultServeMux)
 }
 
 func main() {
+	var TerminateErr = errors.New("Terminated by signal")
 	g, ctx := errgroup.WithContext(context.Background())
-	stop_chan := make(chan struct{})
 
 	sig_chan := make(chan os.Signal, 1)
 	signals := []os.Signal{syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT}
@@ -53,11 +56,11 @@ func main() {
 
 	// init application server
 	g.Go(func() error {
-		return serveApp(stop_chan)
+		return serveApp(ctx)
 	})
 	// init debug server
 	g.Go(func() error {
-		return serveDebug(stop_chan)
+		return serveDebug(ctx)
 	})
 	// go routine that handles termination signals
 	g.Go(func() error {
@@ -67,7 +70,8 @@ func main() {
 					return ctx.Err()
 				case sig := <-sig_chan:
 					fmt.Printf("\n\nSignal received: %v \n", sig)
-					close(stop_chan)
+					message := fmt.Sprintf("Signal received: %v", sig)
+					return errors.Wrapf(TerminateErr, message)
 			}
 		}
 	})
@@ -75,7 +79,7 @@ func main() {
 	// Wait for all HTTP fetches to complete.
 	time.Sleep(time.Second * 1)
 	fmt.Println("awaiting signal")
-	if err := g.Wait(); err != nil && err != http.ErrServerClosed{
+	if err := g.Wait(); err != nil && !errors.Is(err, TerminateErr) {
 		log.Fatal(err)
 	}
 
